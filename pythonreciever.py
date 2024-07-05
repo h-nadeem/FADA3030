b7819f3e9c2a3f8406a448e41c9fbd1d190c591f
Your Name <you@example.com>
2024-07-05 18:55:38 +1000

    changes

--- pythonreciever.py
+++ pythonreciever.py
@@ -4,6 +4,8 @@ import math
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+
+
 def print_volume_handler(unused_addr, args, volume):
   print("[{0}] ~ {1}".format(args[0], volume))
 
@@ -12,17 +14,18 @@ def print_compute_handler(unused_addr, args, volume):
     print("[{0}] ~ {1}".format(args[0], args[1](volume)))
   except ValueError: pass
 
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--ip",
       default="127.0.0.1", help="The ip to listen on")
   parser.add_argument("--port",
-      type=int, default=8338, help="The port to listen on") # faceOSC states port 8338 in the bottom of the window
+      type=int, default=12000, help="The port to listen on") # faceOSC states port 8338 in the bottom of the window
   args = parser.parse_args()
 
   dispatcher = dispatcher.Dispatcher()
   ###########################################
-  dispatcher.map("/raw", print) ## OSC messages are formatted like URLS
+  dispatcher.map("/wek/outputs", print) ## OSC messages are formatted like URLS
   #########################################  check the addresses on the FaceOSC github documentation
   server = osc_server.ThreadingOSCUDPServer(
       (args.ip, args.port), dispatcher)
